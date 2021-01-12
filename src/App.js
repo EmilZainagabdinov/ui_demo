b52f43d9850f16dc2f19bc3374a39ede6abb13da
Emil Zainagabdinov <screw192@gmail.com>
2021-01-12 14:05:32 +0600

    Imported Modal.js, Button.js, Alert.js. Added state for modal and alert. Added functions for toggling modal and alert.
    Returns hidden Modal and Alert components. And Button component with array of two objects containing configurations for buttons.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,65 @@
+import React, {useState} from 'react';
 import './App.css';
+import Modal from "./components/UI/Modal/Modal";
+import Button from "./components/UI/Button/Button";
+import Alert from "./components/UI/Alert/Alert";
 
 const App = () => {
+  const [modalToggled, setModalToggled] = useState(false);
+  const [alertToggled, setAlertToggled] = useState(false);
+
+  const modalShowHandler = () => {
+    setModalToggled(true);
+  };
+
+  const modalHideHandler = () => {
+    setModalToggled(false);
+  };
+
+  const alertShowHandler = () => {
+    setAlertToggled(true);
+  };
+
+  const alertHideHandler = () => {
+    setAlertToggled(false);
+  };
+
   return (
-    <div className="App" />
+      <div className="App">
+        <Modal
+            title="Modal title"
+            show={modalToggled}
+            closed={modalHideHandler}
+            buttonsBlock={[
+              {label: 'Continue', type: 'continue', clicked: (() => {
+                  alertShowHandler();
+                  modalHideHandler();
+                })},
+              {label: 'Close', type: 'danger', clicked: (() => modalHideHandler())}
+            ]}
+        >
+          <p>Some random text for modal pop-up</p>
+          <a
+              href="https://www.instagram.com/screw192/"
+              target="_blank"
+              rel="noreferrer"
+          >
+            And a random link
+          </a>
+        </Modal>
+        <Alert
+            type="primary"
+            show={alertToggled}
+            clickDismissable
+            dismissHandler={alertHideHandler}
+        >
+          <p>This is a success alert</p>
+        </Alert>
+        <Button parameters={[
+            {label: 'Show modal', type: 'success', clicked: (() => modalShowHandler())},
+            {label: 'Show alert', type: 'success', clicked: (() => alertShowHandler())},
+        ]} />
+      </div>
   );
 }
 
